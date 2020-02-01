d6a674a3f43c7f8967c07c12e91b1011265d4d4d
Cas Bitton <casbitton@users.noreply.github.com>
2020-02-01 21:29:12 +1030

    First commit for test release
    Test version is now ready for release

---
+++ New-WindowsServer2019VM.ps1
@@ -0,0 +1,273 @@
+[CmdletBinding()]
+param(
+
+    [Parameter(Mandatory = $true)]
+    [string]$Name,
+
+    [Parameter(Mandatory = $true)]
+    [int64]$CPU,
+
+    [Parameter(Mandatory = $true)]
+    [int64]$Memory,
+
+    [Parameter(Mandatory = $true)]
+    [uint64]$Storage,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('External Switch', 'Internal Switch', 'Private Switch')]
+    [string]$SwitchName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$WindowsISO,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('Windows Server 2019 Datacenter Evaluation', 'Windows Server 2019 Datacenter Evaluation (Desktop Experience)')]
+    [string]$WindowsEdition,
+
+    [string]$AdminPassword
+
+)
+
+# AutoBuild Properties
+
+## Check we have Convert-WindowsImage.ps1
+If (-not (Test-Path -Path .\Convert-WindowsImage.ps1 -PathType Leaf)) {
+    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/MicrosoftDocs/Virtualization-Documentation/master/hyperv-tools/Convert-WindowsImage/Convert-WindowsImage.ps1' -OutFile .\Convert-WindowsImage.ps1
+}
+
+## Random ID
+$BuildID = (Get-Random)
+
+## Base Keys
+## Install with KMS, Activate with AMVA
+$WindowsKMS = 'WMDGN-G9PQG-XVVXX-R3X43-63DFG'
+$WindowsAMVA = 'H3RNG-8C32Q-Q8FRX-6TDXV-WMBMW'
+
+
+## Quick Check for inflight and duplicate builds
+if (Test-Path -Path "C:\Temp\HVA\$Name") {
+    $Name = $Name + $BuildID
+}
+elseif (Get-VM | Where-Object Name -EQ $Name) {
+    $Name = $Name + $BuildID
+}
+
+## Trim and clean name
+$Name = $Name -replace '\s', '' -replace '^(.{0,14}).*', '$1'
+
+## Setup Working Directories for build
+$TempDirectory = "C:\Temp\HVA\$Name"
+$UnattendDirectory = "C:\Temp\HVA\$Name\Unattend"
+New-Item -Path $TempDirectory -ItemType Directory -Force | Out-Null
+New-Item -Path $UnattendDirectory -ItemType Directory -Force | Out-Null
+## Set VHD Path
+$VHDPath = (Get-WmiObject -Namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData).DefaultVirtualHardDiskPath + "\$Name.vhdx"
+
+# User Details
+## Administrator
+$UserName = 'Administrator'
+if (-not $AdminPassword) {
+    $AdminPassword = -join (33..126 | ForEach-Object { [char]$_ } | Get-Random -C 24)
+}
+## Secure String and Credential
+$Password = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($UserName, $Password)
+
+# START BUILD
+
+## Start Time
+$StartTime = Get-Date
+
+Write-Host "[$BuildID] - $(Get-date) - Build Start" -ForegroundColor Green
+Write-Host "[$BuildID] - $(Get-date) - Finding Answers" -ForegroundColor Yellow
+
+## Create Answer File
+$UnattendTemplate = @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="specialize">
+        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <ProductKey></ProductKey>
+            <ComputerName></ComputerName>
+        </component>
+        <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <InputLocale>en-US</InputLocale>
+            <SystemLocale>en-US</SystemLocale>
+            <UserLocale>en-US</UserLocale>
+        </component>
+        <component name="Microsoft-Windows-Security-SPP-UX" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <SkipAutoActivation>true</SkipAutoActivation>
+        </component>
+        <component name="Microsoft-Windows-SQMApi" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <CEIPEnabled>0</CEIPEnabled>
+        </component>
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Path>net user administrator /active:yes</Path>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+    </settings>
+    <settings pass="oobeSystem">
+        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <OOBE>
+                <HideEULAPage>true</HideEULAPage>
+                <HideLocalAccountScreen>true</HideLocalAccountScreen>
+                <HideOEMRegistrationScreen>true</HideOEMRegistrationScreen>
+                <HideOnlineAccountScreens>true</HideOnlineAccountScreens>
+                <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
+                <SkipMachineOOBE>true</SkipMachineOOBE>
+                <SkipUserOOBE>true</SkipUserOOBE>
+            </OOBE>
+            <UserAccounts>
+                <AdministratorPassword>
+                    <Value></Value>
+                    <PlainText>true</PlainText>
+                </AdministratorPassword>
+            </UserAccounts>
+        </component>
+    </settings>
+</unattend>
+'@
+
+## Template Set
+$xml = [xml]$UnattendTemplate
+## Answer destination
+$UnattendPath = "$UnattendDirectory\unattend.xml"
+## Define unattended settings
+$xml.unattend.settings[0].component[0].ComputerName = $Name
+$xml.unattend.settings[0].component[0].ProductKey = $WindowsKMS
+$xml.unattend.settings[1].component.UserAccounts.AdministratorPassword.Value = $AdminPassword
+## Save and Write answers
+$writer = New-Object System.XMl.XmlTextWriter($UnattendPath, [System.Text.Encoding]::UTF8)
+$writer.Formatting = [System.Xml.Formatting]::Indented
+$xml.Save($writer)
+$writer.Dispose()
+
+## Create VHDX from ISO
+Write-Host "[$BuildID] - $(Get-date) - Mixing $Name with $WindowsEdition" -ForegroundColor Yellow
+
+## Import Convert-WindowsImage.ps1 function for use
+. .\Convert-WindowsImage.ps1
+
+## Compile Image
+Convert-WindowsImage -SourcePath $WindowsISO -Edition $WindowsEdition -TempDirectory $TempDirectory -UnattendPath $UnattendPath -SizeBytes $Storage -DiskLayout UEFI -VHDPath $VHDPath -VHDFormat VHDX
+
+## Setup new VM
+$SetupVM = New-VM -Name $Name -Generation 2 -MemoryStartupBytes $Memory -VHDPath $VHDPath -SwitchName $SwitchName
+$SetupVM | Set-VMProcessor -Count $CPU
+$SetupVM | Get-VMIntegrationService -Name "Guest Service Interface" | Enable-VMIntegrationService -Passthru
+$SetupVM | Set-VMMemory -DynamicMemoryEnabled $true
+$SetupVM | Set-VM -AutomaticCheckpointsEnabled $false
+$SetupVM | Start-VM
+
+## Wait for installation complete
+Wait-VM -Name $Name -For Heartbeat
+
+## START CONFIG ##
+
+do {
+    Start-Sleep -Seconds 60
+    $StartSession = New-PSSession -VMName $Name -Credential $Credential -ErrorAction SilentlyContinue
+} until ($StartSession.State -eq "Opened")
+
+## Connect to New VM and enable Remote Management
+Write-Host "[$BuildID] - $(Get-date) - Setting up $Name" -ForegroundColor Yellow
+
+## Setup Ansible for Host Management
+Write-Host "[$BuildID] - $(Get-date) - Enabling Ansible Management on $Name" -ForegroundColor Yellow
+Invoke-Command -Session $StartSession -ScriptBlock {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
+}
+ 
+## Install Chocolatey
+Write-Host "[$BuildID] - $(Get-date) - Installing Chocolatey on $Name" -ForegroundColor Yellow
+Invoke-Command -Session $StartSession -ScriptBlock {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+## Setup RDP for Host Management if Desktop
+if ($WindowsEdition -match "Desktop") {
+    Write-Host "[$BuildID] - $(Get-date) - Enabling RDP on $Name" -ForegroundColor Yellow
+    Invoke-Command -Session $StartSession -ScriptBlock {
+        Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+        Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+    }
+
+    ## Upgrade OS from Eval to Full using KMS Key
+    ## https://docs.microsoft.com/en-us/windows-server/get-started/kmsclientkeys
+    Write-Host "[$BuildID] - $(Get-date) - Converting Windows Server Evaluation to Full" -ForegroundColor Yellow
+    Invoke-Command -Session $StartSession -ScriptBlock {
+        dism /online /Set-Edition:ServerDatacenter /ProductKey:WMDGN-G9PQG-XVVXX-R3X43-63DFG /AcceptEula /NoRestart /Quiet
+    }
+    
+}
+else {
+ 
+    ## Upgrade OS from Eval to Full using KMS Key
+    ## https://docs.microsoft.com/en-us/windows-server/get-started/kmsclientkeys
+    Write-Host "[$BuildID] - $(Get-date) - Converting Windows Server Evaluation to Full" -ForegroundColor Yellow
+    Invoke-Command -Session $StartSession -ScriptBlock {
+        dism /online /Set-Edition:ServerDatacenterCor /ProductKey:WMDGN-G9PQG-XVVXX-R3X43-63DFG /AcceptEula /NoRestart /Quiet
+    }
+    
+}
+
+## Reboot
+Write-Host "[$BuildID] - $(Get-date) - Restarting $Name" -ForegroundColor Yellow
+Invoke-Command -Session $StartSession -ScriptBlock {
+    shutdown /r
+}
+
+## Create new session after reboot and continue
+Wait-VM -Name $Name -For Heartbeat
+do {
+    Start-Sleep -Seconds 60
+    $StartSession2 = New-PSSession -VMName $Name -Credential $Credential -ErrorAction SilentlyContinue
+} until ($StartSession2.State -eq "Opened")
+
+## Set AVMA Key for Windows Server 2019
+## https://docs.microsoft.com/en-us/windows-server/get-started-19/vm-activation-19
+Write-Host "[$BuildID] - $(Get-date) - Setting AVMA Key on $Name" -ForegroundColor Yellow
+Invoke-Command -Session $StartSession2 -ScriptBlock {
+    cscript.exe $env:SystemRoot\System32\slmgr.vbs /ipk H3RNG-8C32Q-Q8FRX-6TDXV-WMBMW
+    # Activate Windows
+    cscript.exe $env:SystemRoot\System32\slmgr.vbs /ato
+    # Return activation state
+    cscript.exe $env:SystemRoot\System32\slmgr.vbs /dli
+}
+
+## Disable Realtime Antivirus monitoring, Clean install
+Write-Host "[$BuildID] - $(Get-date) - Cleaning up $Name" -ForegroundColor Yellow
+Invoke-Command -Session $StartSession2 -ScriptBlock {
+    # Disable realtime monitoring
+    Set-MpPreference -DisableRealtimeMonitoring $true
+
+    # Clean up OS
+    dism /online /Cleanup-Image /StartComponentCleanup /ResetBase /Quiet
+}
+
+## Reboot
+Write-Host "[$BuildID] - $(Get-date) - Restarting $Name" -ForegroundColor Yellow
+Invoke-Command -Session $StartSession2 -ScriptBlock {
+    shutdown /r
+}
+
+# Waiting for Heartbeat prior to finish
+Wait-VM -Name $Name -For Heartbeat
+
+# END CONFIG
+
+## End Time
+$EndTime = Get-Date
+$TotalTime = (New-TimeSpan -Start $StartTime -End $EndTime).Minutes
+
+# END BUILD
+
+## Wrap it up
+Write-Host "[$BuildID] - $(Get-date) - $Name is now ready"
+Write-Host "[$BuildID] - $(Get-date) - Credentials: $UserName\$AdminPassword" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Build End" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Completed in $TotalTime Minutes" -ForegroundColor Green
--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# HVA-WindowsServer2019
-Automated Hyper-V Deployment for Windows Server 2019
+## Overview
+
+Deploy new Windows Server 2019 virtual machines on demand while you drink a beer.
+
+This is a highly opinionated base install specific to my Intel NUC [lab](#Further-Notes). You might find it useful.
+
+Gremlins may appear after dark if used in production environments.
+
+Cheers! 🍻
+
+---
+
+## Prerequisites:
+* A host Server running - Windows Server 2019 Datacenter with [Hyper-V Role](https://docs.microsoft.com/en-us/windows-server/virtualization/hyper-v/get-started/install-the-hyper-v-role-on-windows-server)
+* Adequate storage and memory for builds
+* 3 Virtual Switches configured via [Hyper-V Manager](https://docs.microsoft.com/en-us/windows-server/virtualization/hyper-v/get-started/create-a-virtual-switch-for-hyper-v-virtual-machines). Each Virtual Switch will be named:
+  * External Switch
+  * Internal Switch
+  * Private Switch
+* Windows Server 2019 ISO from [Microsoft Evaluation Center](https://www.microsoft.com/en-us/evalcenter/evaluate-windows-server)
+
+#### Optional Cloud Enhancement ☁️🚀🛰
+* This can be paired with [GitLab](https://about.gitlab.com/) or [Azure DevOps](https://azure.microsoft.com/en-us/services/devops/) to yield some fantastic results
+* Install a runner from your favorite CI/CD platform of choice, and the deployment process can be performed entirely from any browser 👌
+
+---
+
+## Supported Operating Systems:
+* Windows Server 2019 Datacenter Evaluation - ServerDatacenterCor
+* Windows Server 2019 Datacenter Evaluation (Desktop Experience) - ServerDatacenter
+
+## Each new build includes:
+* Built from Source each run
+* Automatic virtual machine activation
+* Configured for use with 
+  * [Ansible](https://github.com/ansible/ansible)
+  * [Chocolatey](https://github.com/chocolatey/choco)
+* Lightning fast deploy on modern hardware
+  * Desktop Experience , ~10 Minutes
+  * Core , ~5 Minutes
+
+---
+
+## Getting Started
+
+This is short and sweet, enjoy 😀
+
+`New-WindowsServer2019VM.ps1` has minimal options, this may be expanded upon in the future. 
+
+- Name
+- CPU
+- Memory
+- Storage
+- SwitchName
+- WindowsISO
+- WindowsEdition
+- AdminPassword (Optional)
+
+**Security Note:** _The administrator password will be generated for you if not selected. This will be output to you in plaintext upon build completion in either scenario._
+
+## Example Build
+
+```
+.\New-WindowsServer2019VM.ps1 -Name Server01415077 -CPU 2 -Memory 4GB -Storage 40GB -SwitchName 'External Switch' -WindowsISO C:\Temp\17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso -WindowsEdition 'Windows Server 2019 Datacenter Evaluation'
+```
+
+_A few moments later_
+
+```
+[415077964] - 02/01/2020 20:33:11 - Build Start
+[415077964] - 02/01/2020 20:33:11 - Finding Answers
+[415077964] - 02/01/2020 20:33:11 - Mixing Server01415077 with Windows Server 2019 Datacenter Evaluation
+[415077964] - 02/01/2020 20:35:30 - Setting up Server01415077
+[415077964] - 02/01/2020 20:35:30 - Enabling Ansible Management on Server01415077
+[415077964] - 02/01/2020 20:35:43 - Installing Chocolatey on Server01415077
+[415077964] - 02/01/2020 20:35:53 - Converting Windows Server Evaluation to Full
+[415077964] - 02/01/2020 20:36:14 - Restarting Server01415077
+[415077964] - 02/01/2020 20:37:18 - Setting AVMA Key on Server01415077
+[415077964] - 02/01/2020 20:37:26 - Cleaning up Server01415077
+[415077964] - 02/01/2020 20:37:46 - Restarting Server01415077
+[415077964] - 02/01/2020 20:37:46 - Server01415077 is now ready
+[415077964] - 02/01/2020 20:37:46 - Credentials: Administrator\p/!xPaw6lfIK{g3QLTc(h8S<
+[415077964] - 02/01/2020 20:37:46 - Build End
+[415077964] - 02/01/2020 20:37:46 - Completed in 4 Minutes
+```
+
+---
+
+### Further Notes
+
+* [1] Tested on NUC7i7BNH with NVMe Storage
+* Microsoft [Virtualization-Documentation](https://github.com/MicrosoftDocs/Virtualization-Documentation)
